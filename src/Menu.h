4063cd631078c2e1eb55eded4fb92c80335ee55e
Tiago Torres <tiagoftorres@gmail.com>
2024-03-17 20:59:16 +0000

    options written

--- src/Menu.h
+++ src/Menu.h
@@ -3,30 +3,16 @@
 
 #include <string>
 #include <vector>
-#include "Graph.h"
+#include "Management.h"
 
-/**
- * @brief Terminal auxiliary struct storing the sorting options of a table.
- */
-struct sortingOptions {
-    /**
-     * Column, currently displaying, to order by
-     */
-    int orderBy = 0;
-    bool ascending = true;
-};
 
 /**
  * @brief Terminal auxiliary struct storing the printing options for a printing function.
  */
 struct printingOptions {
-    sortingOptions sortOptions;
     std::string message;
     bool clear = true;
     bool printMessage = true;
-    bool printCountMessage = true;
-    bool sort = true;
-    bool showSortingOptions = true;
     bool showEndMenu = true;
     bool getInput = true;
 };
@@ -40,7 +26,7 @@ private:
     /**
      * @brief Graph containing all the air travelling information that is being managed
      */
-    Graph g;
+    Management m;
 
     // Table column widths
     const static int MENU_WIDTH = 75;
@@ -52,20 +38,18 @@ private:
     const static int AIRLINE_WIDTH = 9;
 
 public:
-    Terminal(const Graph &g);
+    Menu(const Management &m);
     void run();
 
 private:
     // Wait for inputs
     void waitMenu();
     char getInput();
-    void getSortingOptions(sortingOptions &sortOptions);
 
     // Print menus
     void printMainMenu();
     void endDisplayMenu();
     void printBackToMenu();
-    void printSortingOptions();
     void printExit();
 
     // Auxiliary formatting functions
@@ -84,16 +68,6 @@ private:
     void printCountriesList(std::vector<std::string> countries, printingOptions options);
     void printSourceDestList(std::vector<std::pair<Airport *, Airport *>> sourceDestPairs, printingOptions options);
 
-    // Sorting lists
-    void sortAirportsList(std::vector<Airport *> &airports, sortingOptions sortOptions);
-    void sortAirlinesList(std::vector<Airline *> &airlines, sortingOptions sortOptions);
-    void sortFlightsList(std::vector<Flight> &flights, sortingOptions sortOptions);
-    void sortCitiesList(std::vector<std::pair<std::string, std::string>> &cities, sortingOptions sortOptions);
-    void sortCountriesList(std::vector<std::string> &countries, sortingOptions sortOptions);
-
-    // Auxiliary sorting functions
-    static bool byCity(std::pair<std::string, std::string> p1, std::pair<std::string, std::string> p2);
-    static bool byCountry(std::pair<std::string, std::string> p1, std::pair<std::string, std::string> p2);
 };
 
 
