44eac49276ea53a8a10111ad96a2c16646960bf1
Pedro <pedrodfjoao@gmail.com>
2024-03-17 19:30:37 +0000

    management

---
+++ src/Management.h
@@ -0,0 +1,13 @@
+
+#ifndef PROJECT1_MANAGEMENT_H
+#define PROJECT1_MANAGEMENT_H
+#include "Graph.h"
+
+class Management {
+public:
+    getMaxFlow();
+    getMaxFlowCity();
+};
+
+
+#endif //PROJECT1_MANAGEMENT_H
